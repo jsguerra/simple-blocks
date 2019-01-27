3bf7cdea892ece8a9055e764a270f18ae5f0bd61
Jose Guerra <jose@gcostudios.com>
2019-01-27 07:38:19 +0000

    added function to add class to featured image article

--- wp-simple-min/js/child-blocks.js
+++ wp-simple-min/js/child-blocks.js
@@ -5,7 +5,7 @@
     var $hasFeatured = $('.with-featured-image .featured-image img');
 
     if ($hasFeatured.length) {
-      $('.with-featured-image').append('');
+      $('.with-featured-image').addClass('has-wide-image');
     }
 	});
 
