4b7edeecfa9536c5997ecc50dc921284dafa4db6
Jose Guerra <jose@gcostudios.com>
2019-01-27 19:44:28 +0000

    disabled js

--- wp-simple-min/functions.php
+++ wp-simple-min/functions.php
@@ -15,6 +15,6 @@ function atomic_blocks_parent_theme_style() {
   wp_enqueue_style( 'atomic-blocks-child-style', get_stylesheet_directory_uri(). '/style.css' );
   
   // Child theme js
-  wp_enqueue_script( 'script', get_stylesheet_directory_uri() . '/js/child-blocks.js', array ( 'jquery' ), 1.1, true);
+  //wp_enqueue_script( 'script', get_stylesheet_directory_uri() . '/js/child-blocks.js', array ( 'jquery' ), 1.1, true);
 }
 add_action( 'wp_enqueue_scripts', 'atomic_blocks_parent_theme_style' );
