9cc0c51cd60ad81b577c983128b8226d3e347382
Jose Guerra <jose@gcostudios.com>
2019-06-15 21:45:53 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+(function($) {
+	"use strict";
+
+	$(document).ready(function() {
+    var $hasFeatured = $('.with-featured-image .featured-image img');
+
+    if ($hasFeatured.length) {
+      $('.with-featured-image').addClass('has-wide-image');
+    }
+	});
+
+})(jQuery);
